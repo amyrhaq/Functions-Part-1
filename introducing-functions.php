4809893900845c584ad8ffd2624c893aa1d5d2d8
Your Name Here <amyr@amyrhaq.com>
2013-09-26 13:37:50 -0700

    completed lesson 1/8

---
+++ introducing-functions.php
@@ -0,0 +1,14 @@
+<!-- Get the length of your own name, as I did for mine, and print it to the screen. -->
+<html>
+<head>
+	<title>Introducing Functions</title>
+</head>
+<body>
+<p>
+	<?php
+	$length = strlen("amyr");
+	echo $length;
+	?>
+</p>
+</body>
+</html>
